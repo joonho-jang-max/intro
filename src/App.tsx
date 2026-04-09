466df58eeb227ff8ae943d0ca1e906c111ed9eb1
USER <user@AD02375355.local>
2026-04-09 22:05:33 +0900

    Migrate search-redesign content into test3 structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './index.css'
 import Component from './Component'
+import FloatingCat from './FloatingCat'
 
 function App() {
-  return <Component />
+  return (
+    <>
+      <div style={{ maxWidth: 375, margin: '0 auto', background: '#f7f7f7', minHeight: '100dvh', position: 'relative' }}>
+        <Component />
+      </div>
+      <FloatingCat />
+    </>
+  )
 }
 
 export default App
