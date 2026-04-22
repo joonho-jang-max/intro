f31a9da3a9a5cb761f46510b60de4b7db51cdae1
USER <user@AD02375355.local>
2026-04-22 15:44:30 +0900

    set base to /intro/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/test3/',
+  base: '/intro/',
   plugins: [react(), tailwindcss()],
 })
